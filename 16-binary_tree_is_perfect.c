26979550e50d1b50594a6b54dac8586a977c9f49
charles igboanugo <charlesigboanugo033@gmail.com>
2024-09-17 16:05:11 +0100

    great

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,35 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_full - counts the leaves in a binary tree
+ * @tree: parent node
+ *
+ * Return: returns the number of leave nodes
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left;
+	int right;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else if (tree->left != NULL && tree->right != NULL)
+	{
+		left = binary_tree_is_full(tree->left);
+		right = binary_tree_is_full(tree->right);
+	}
+	else
+		return (0);
+
+	if (left && right)
+		return (1);
+	else
+		return (0);
+}
 
 /**
  * dept_deepest_leaf - counts the leaves in a binary tree
@@ -52,14 +81,16 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int perfect;
+	int balance_factor;
+	int full;
 
 	if (!tree)
 		return (0);
 
-	perfect = binary_tree_balance(tree);
-	if (perfect != 0)
-		return (0);
-	else
+	balance_factor = binary_tree_balance(tree);
+	full = binary_tree_is_full(tree);
+	if (balance_factor == 0 && full == 1)
 		return (1);
+	else
+		return (0);
 }
