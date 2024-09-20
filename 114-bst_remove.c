83a74d99ecc1f00e2dd52a38d728b63a39a04fa5
charles <charlezchucks@gmail.com>
2024-09-20 13:36:42 +0000

    114update

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -2,6 +2,33 @@
 #include "binary_trees.h"
 
 /**
+ * bst_remove_node_with_2_child - Removes a node that has two children from a
+ * binary search tree without actually deleting the node (by swapping values)
+ * @node: Pointer to the node to remove
+ */
+void bst_remove_node_with_2_child(bst_t *node)
+{
+	bst_t *successor = NULL;
+	bst_t *parent = NULL;
+
+	successor = node->right;
+	parent = node;
+	while (successor->left)
+	{
+		parent = successor;
+		successor = successor->left;
+	}
+	node->n = successor->n;
+	if (parent == node)
+		parent->right = successor->right;
+	else
+		parent->left = successor->right;
+	if (successor->right)
+		successor->right->parent = parent;
+	free(successor);
+}
+
+/**
  * bst_remove - Removes a node from a Binary Search Tree.
  * @root: Pointer to the root node of the tree.
  * @value: The value to remove from the tree.
@@ -10,10 +37,13 @@
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *node = root, *parent, *successor = NULL, *temp = NULL;
+	bst_t *node = root, *parent = NULL, *temp = NULL;
 
-	for (parent = NULL; node && node->n != value; parent = node)
+	while (node && node->n != value)
+	{
+		parent = node;
 		node = (value < node->n) ? node->left : node->right;
+	}
 	if (!node)
 		return (root);
 	if (!node->left && !node->right)
@@ -39,15 +69,7 @@ bst_t *bst_remove(bst_t *root, int value)
 		free(node);
 	}
 	else
-	{
-		successor = node->right;
-		for (parent = node; successor->left; parent = successor)
-			successor = successor->left;
-		node->n = successor->n;
-		parent->left = successor->right;
-		if (successor->right)
-			successor->right->parent = parent;
-		free(successor);
-	}
+		bst_remove_node_with_2_child(node);
+
 	return (root);
 }
