5a68c1f7590c7afb375f284e5ef60d0d3b11e04c
charles <charlezchucks@gmail.com>
2024-09-22 14:26:57 +0000

    123

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -3,30 +3,36 @@
 
 
 /**
- * avl_balance_node_removal - balances an unbalanced node after node removal
+ * avl_balance_node - balances an unbalanced node after node
  * @node: Pointer to the node to balance.
- * @value: value removed from tree
  *
  * Return: pointer to the balanced node
  */
-avl_t *avl_balance_node_removal(avl_t *node, int value)
+avl_t *avl_balance_node(avl_t *node)
 {
 	int balance;
 
 	balance = binary_tree_balance(node);
-	if (balance > 1 && value > node->right->n)
-		node = binary_tree_rotate_right(node);
-	else if (balance > 1 && value < node->right->n)
+
+	if (balance > 1)
 	{
-		node->right = binary_tree_rotate_left(node->right);
-		node = binary_tree_rotate_right(node);
+		if (binary_tree_balance(node->left) >= 0)
+			node = binary_tree_rotate_right(node);
+		else
+		{
+			node->left = binary_tree_rotate_left(node->left);
+			node = binary_tree_rotate_right(node);
+		}
 	}
-	else if (balance < -1 && value < node->left->n)
-		node = binary_tree_rotate_left(node);
-	else if (balance < -1 && value > node->left->n)
+	if (balance < -1)
 	{
-		node->left = binary_tree_rotate_right(node->left);
-		node = binary_tree_rotate_left(node);
+		if (binary_tree_balance(node->right) <= 0)
+			node = binary_tree_rotate_left(node);
+		else
+		{
+			node->right = binary_tree_rotate_right(node->right);
+			node = binary_tree_rotate_left(node);
+		}
 	}
 	return (node);
 }
@@ -42,20 +48,18 @@ avl_t *avl_balance_node_removal(avl_t *node, int value)
 avl_t *avl_remove_node_with_2_child(avl_t *node, bst_t *successor)
 {
 	bst_t *temp = NULL;
-	int value;
 
 	if (!successor->left)
 	{
 		node->n = successor->n;
-		value = successor->n;
 		temp = successor->right;
 		if (temp)
 			temp->parent = successor->parent;
-		free(successor)
-		return (temp)
+		free(successor);
+		return (temp);
 	}
 	successor->left = avl_remove_node_with_2_child(node, successor->left);
-	successor = avl_balance_node_removal(successor, value);
+	successor = avl_balance_node(successor);
 	return (successor);
 }
 
@@ -63,23 +67,23 @@ avl_t *avl_remove_node_with_2_child(avl_t *node, bst_t *successor)
  * avl_remove_recursion - Removes a node from an AVL Tree.
  * @node: Pointer to the root node of the tree.
  * @value: The value to remove from the tree.
- * @value_found: indicates whether a node is removed.
+ * @found: indicates whether a node is removed.
  *
  * Return: Pointer to the new root node of the tree after removal.
  */
-avl_t *avl_remove_recursion(bst_t *node, int value, int *value_found)
+avl_t *avl_remove_recursion(bst_t *node, int value, int *found)
 {
 	bst_t *temp = node;
 
 	if (!node)
 		return (node);
 	if (value > node->n)
-		node->right = avl_remove_recursion(node->right, value);
+		node->right = avl_remove_recursion(node->right, value, found);
 	else if (value < node->n)
-		node->left = avl_remove_recursion(node->left, value);
+		node->left = avl_remove_recursion(node->left, value, found);
 	else
 	{
-		*value_found = 1;
+		*found = 1;
 		if (!node->left && !node->right)
 		{
 			node = NULL;
@@ -97,8 +101,8 @@ avl_t *avl_remove_recursion(bst_t *node, int value, int *value_found)
 			node->right = temp;
 		}
 	}
-	if (value_found)
-		node = avl_balance_node_removal(node, value);
+	if (node && *found)
+		node = avl_balance_node(node);
 	return (node);
 }
 
@@ -111,9 +115,9 @@ avl_t *avl_remove_recursion(bst_t *node, int value, int *value_found)
  */
 avl_t *avl_remove(bst_t *root, int value)
 {
-	int value_found = 0;
+	int found = 0;
 
-	if (!node)
+	if (!root)
 		return (NULL);
-	return (avl_remove_recursion(root, value, &value_found));
+	return (avl_remove_recursion(root, value, &found));
 }
